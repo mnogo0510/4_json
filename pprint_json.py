43bc683835520b89c048f6c6eee4b686cd072655
aaa bbb <mnogo0510@gmail.com>
2017-06-26 17:26:47 +0300

    Реализован наглядный вывод json из файла в консоль

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,60 @@
 import json
+import sys
+from pprint import pprint
+import argparse
 
+version = "1.0.0"
+
+def createParser (params):
+
+    if not params:
+        params = ['-h']
+
+    parser = argparse.ArgumentParser(
+            prog='Pretty print',
+            description = '''
+            This program demonstrates pretty print of json''',
+            epilog='''(c) Moscow 2017''',
+            add_help=False
+            )
+
+    parser.add_argument('TEST', help='Positional parameter')
+
+    parent_group = parser.add_argument_group (title='Paramaters')
+
+    parent_group.add_argument('--help', '-h', action='help', help='Help')
+
+
+
+    parent_group.add_argument ('--version',
+            action='version',
+            help = 'Version number',
+            version='%(prog)s {}'.format (version))
+
+    parser.add_argument('-f', '--file', type=argparse.FileType())
+
+
+
+
+    return parser.parse_args(params)
 
 def load_data(filepath):
-    pass
+    jsObj = open(filepath, "r")
+    pObj = json.load(jsObj)
+    return pObj
 
 
 def pretty_print_json(data):
-    pass
+    pprint(data)
 
 
 if __name__ == '__main__':
-    pass
+    parserCommandLine = createParser(sys.argv[1:])
+
+    print(parserCommandLine.TEST)
+    if parserCommandLine.file:
+        pObj = load_data(parserCommandLine.file.name)
+        pretty_print_json(pObj)
+
+
+
